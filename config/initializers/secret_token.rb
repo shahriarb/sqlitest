4290b1ff4df0143bf685063432eb696a86aa1a21
ShaB <shab@ubuntu.(none)>
2013-01-21 14:03:05 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sqlittest::Application.config.secret_token = 'bdd766c0cdfb54f25c64ec7a526a9e662c5d16d3be185295ebb427320e945eac56c6bf00a52b925f2fcd5ed3a63d9d63ad71884cf6d12f349a873c7ccbbfb912'
